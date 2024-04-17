8068e1a34d98863233d916628660839a56e26e57
Glauco Knihs <eglauko@hotmail.com>
2024-04-16 23:38:39 -0300

    idealizando

---
+++ RoyalCode.SmartValidations/IRule.cs
@@ -0,0 +1,13 @@
+﻿using RoyalCode.OperationResults;
+
+namespace RoyalCode.SmartValidations;
+
+public interface IRule<in TModel, in TDependecy>
+{
+    ValidableResult Validate(TModel model, TDependecy dependency, ValidableResult result);
+}
+
+public interface IRule<in TModel>
+{
+    ValidableResult Validate(TModel model, ValidableResult result);
+}
---
+++ RoyalCode.SmartValidations/Validations.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq.Expressions;
+using RoyalCode.OperationResults;
+
+namespace RoyalCode.SmartValidations;
+
+public static class Validations
+{
+    public static ValidatorBuilder<TModel> For<TModel>()
+    {
+        return new ValidatorBuilder<TModel>();
+    }
+}
+
+public class ValidatorBuilder<TModel>
+{
+    private readonly List<IRule<TModel>> rules = [];
+    
+    public ValidatorBuilder<TModel> AddRule(IRule<TModel> rule)
+    {
+        rules.Add(rule);
+        return this;
+    }
+}
+
+public class PropertyExpressions
+{
+    
+}
+
+public class PropertyValidatorBuilder<TModel, TProperty>
+{
+    private readonly ValidatorBuilder<TModel> validatorBuilder;
+    private readonly Expression<Func<TModel, TProperty>> propertyExpression;
+
+    public PropertyValidatorBuilder(ValidatorBuilder<TModel> validatorBuilder, 
+        Expression<Func<TModel, TProperty>> propertyExpression)
+    {
+        this.validatorBuilder = validatorBuilder;
+        this.propertyExpression = propertyExpression;
+    }
+    
+    
+}
+
+public interface IPropertyRule<in TModel, in TProperty>
+{
+    public ValidableResult Validate(TModel model, TProperty property, ValidableResult result);
+}
